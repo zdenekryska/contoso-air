daf1618c21d76792444e49558285735206dcfc27
Zdenek Ryska <zdenek.ryska@aveva.com>
2025-05-22 09:36:13 -0700

    feat: passwordless authentication for azure cosmosdb

--- src/web/repositories/book.repository.js
+++ src/web/repositories/book.repository.js
@@ -1,14 +1,41 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const axios = require("axios");
 const mongoose = require("mongoose");
 const UserInfoModelSchema = require("./book.repository.model");
 
 class BookFlightsRepository {
   constructor(options) {
-    const initialize = async () => {
-      throw new Error("Database connection not implemented");
-    };
+    const { listConnectionStringUrl, scope, clientId } = options;
+    let accessToken;
+    let connectionString;
 
-    initialize().catch((error) => {
-      console.error("Failed to connect to the database: ", error);
+    initialize().catch(async (error) => {
+      // Get the access token for the managed identity
+      const credential = new DefaultAzureCredential({
+        managedIdentityClientId: clientId,
+      });
+      accessToken = await credential.getToken(scope);
+
+      // Get the connection string using the access token
+      const config = {
+        method: "post",
+        url: listConnectionStringUrl,
+        headers: {
+          Authorization: `Bearer ${accessToken.token}`,
+        },
+      };
+      const response = await axios(config);
+      const keysDict = response.data;
+      connectionString = keysDict["connectionStrings"][0]["connectionString"];
+
+      // Connect to the MongoDB server using the connection string
+      mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      mongoose.Promise = global.Promise;
+
+      console.log("Connected to the database");
     });
   }
 
